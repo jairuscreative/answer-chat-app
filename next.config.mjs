e0fd25b3081955bd1b12c40338bec7a97b568dc9
jairuscreative <peter@jairuscreative.com>
2025-02-27 22:11:26 -0600

    Update next.config.mjs
    changed to root directy

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    basePath: "/answer",
+    basePath: "",
     experimental: {
       serverActions: {
         allowedOrigins: ["demo.exa.ai"],
