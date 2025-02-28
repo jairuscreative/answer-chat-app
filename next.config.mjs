152049f6ac7cd124c84fc92cddf1fc414103ec51
jairuscreative <peter@jairuscreative.com>
2025-02-27 21:48:51 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    basePath: "/",
+    basePath: "/answer",
     experimental: {
       serverActions: {
         allowedOrigins: ["demo.exa.ai"],
