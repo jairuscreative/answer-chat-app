a143fc87ed513c31d60007dabd48b2e83400974b
jairuscreative <peter@jairuscreative.com>
2025-02-27 21:41:59 -0600

    Update layout.tsx
    Changed title of the site.

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ const reckless = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Exa Answer Chat App",
+  title: "Get Answered at TulsaAnswers.com",
   description: "An opensource chat application built with Exa Answer endpoint.",
   openGraph: {
     title: "Exa Answer Chat App",
